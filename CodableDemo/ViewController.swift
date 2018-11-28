3f10c51203edb93d33d241c5160fdedce3dafd50
manish <manish026@live.com>
2018-11-28 10:34:52 +0530

    Initial Commit

---
+++ CodableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodableDemo
+//
+//  Created by Manish on 28/11/18.
+//  Copyright © 2018 Manish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
